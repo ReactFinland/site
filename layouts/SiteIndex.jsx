ca95f9bfba5524ec63326ab962b0748e28d4b447
Juho Vepsalainen <bebraw@gmail.com>
2017-09-21 17:29:46 +0200

    chore: Add initial header
    This needs more work.

--- layouts/SiteIndex.jsx
+++ layouts/SiteIndex.jsx
@@ -1,71 +1,36 @@
 import React from "react";
-import { LatestPost, SocialLinks } from "@survivejs/components";
+import { SocialLinks } from "@survivejs/components";
 
-const SiteIndex = ({ section }) =>
+const SiteIndex = ({ section }) => (
   <div className="frontpage">
     <div className="front__heading">
       <div className="front-heading-content-wrapper">
         <div className="front-header-wrapper">
           <div className="front-name">
-            <span className="first">Survive</span>
-            <span className="second">JS</span>
+            <span className="first">React </span>
+            <span className="second">Finland</span>
           </div>
 
-          <h1 className="front-header">Learn Webpack and React</h1>
+          <h1 className="front-header">
+            One workshop day + two days of presentations
+          </h1>
           <h3 className="front-motto">
-            SurviveJS will take you from apprentice to master
+            Your chance to learn more about React up north
           </h3>
-
-          <div className="front-button-wrapper">
-            <a
-              className="btn btn--buy-main"
-              href="https://leanpub.com/survivejs-react"
-            >
-              Become a React master
-            </a>
-            <span className="buy-or">or</span>
-            <a
-              className="btn btn--buy-main"
-              href="https://leanpub.com/survivejs-webpack"
-            >
-              Become a Webpack master
-            </a>
-            <span className="buy-or">
-              (or{" "}
-              <a href="https://leanpub.com/b/survivejs-webpack-react">both</a>!)
-            </span>
-          </div>
-
-          <div className="front-button-wrapper">
-            <div className="read-free-note">
-              There are also free versions available!
-            </div>
-            <span className="read-free">
-              <a href="/react/introduction/">
-                Read the free version of the React book
-              </a>
-              <span> or </span>
-              <a href="/webpack/preface/">
-                read the free version of the webpack book
-              </a>
-            </span>
-          </div>
         </div>
       </div>
     </div>
 
     <div className="post post--front">
       <section className="post__content">
-        <LatestPost section={section} />
         <div dangerouslySetInnerHTML={{ __html: require("./index.md").body }} />
 
         <SocialLinks />
-
-        <LatestPost section={section} />
       </section>
     </div>
-  </div>;
+  </div>
+);
 SiteIndex.description =
-  "Want to learn webpack or React? Get started for free and build a Kanban board by following the example project.";
+  "React Finland is a conference that comes with a workshop day and two days of presentations.";
 
 export default SiteIndex;
