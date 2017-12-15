34ef0f63336c1ee611525870b4cad176d3065b91
Juho Vepsalainen <bebraw@gmail.com>
2017-12-15 13:27:32 +0100

    chore: Drop dead code

--- components/AnchorHeader.js
+++ components/AnchorHeader.js
@@ -19,19 +19,3 @@ const AnchorHeader = ({ level, children }) => {
 };
 
 export default AnchorHeader;
-
-// patch ids (this.options.headerPrefix can be undefined!)
-/*
-renderer.heading = function heading(text, level, raw) {
-  const id = raw.toLowerCase().replace(/`/g, "").replace(/[^\w]+/g, "-");
-
-  return (
-    `<h${level} class="header">` +
-    `<a class="header-anchor" href="#${id}" id="${id}"></a>` +
-    `<span class="text">${text}</span>` +
-    `<a class="header-anchor-select" href="#${id}">#</a>` +
-    `</h${level}>\n`
-  );
-};
-
-*/
