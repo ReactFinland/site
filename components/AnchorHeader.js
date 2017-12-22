230c902fc481f4cbbe0cbed05858aa974c472f95
Juho Vepsalainen <bebraw@gmail.com>
2017-12-22 15:20:44 +0200

    fix: Make sure anchors have unique ids
    Otherwise they won't work in a nice way. Maybe there's a better
    solution. Now it relies on global state that doesn't behave well during
    development. Likely this logic has to be extracted somehow.

--- components/AnchorHeader.js
+++ components/AnchorHeader.js
@@ -1,10 +1,22 @@
 import React from "react";
 import slugify from "./slugify";
 
+// XXXXX: Global store to avoid generating duplicate ids. Push this elsewhere?
+const ids = {}; // { id: index }. Example: { foo: 1 }
+
 const AnchorHeader = ({ level, anchor, children }) => {
-  const id = slugify(
+  let id = slugify(
     anchor || (Array.isArray(children) ? children : [children]).join()
   );
+
+  // Check for duplicate id
+  if (ids[id]) {
+    ids[id]++;
+
+    id += `-${ids[id]}`;
+  } else {
+    ids[id] = 1;
+  }
 
   return React.createElement(`h${level}`, { className: "header" }, [
     <a className="header-anchor" href={`#${id}`} id={id} key="anchor" />,
