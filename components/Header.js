f253aa5b47321ffe03bdcaaec255797a727fd7b3
Juho Vepsalainen <bebraw@gmail.com>
2018-01-17 11:45:42 +0100

    fix: Make the header more compact on other pages than index

--- components/Header.js
+++ components/Header.js
@@ -11,9 +11,8 @@ const Header = ({ pathname, logo }) => (
         <span className="text-rf-blue">Finland</span>
       </div>
       <h1>One workshop day + two days of talks</h1>
-      <h3>
-        Your chance to learn more about React up north - Helsinki (24-26.4.2018)
-      </h3>
+      <h3>Your chance to learn more about React up north</h3>
+      <h3>Helsinki (24-26.4.2018)</h3>
       <div className="buy">
         <a href="https://ti.to/react-finland/2018" className="buy-button">
           Buy tickets now!
