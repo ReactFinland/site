007fe1a811fe237fa242586ee938a400fe4a196f
Andrey Okonetchnikov <andrey@okonet.ru>
2018-01-22 16:23:19 +0100

    Simplify headings markup and CSS and make the page background white

--- components/AnchorHeader.js
+++ components/AnchorHeader.js
@@ -4,13 +4,9 @@ import PropTypes from "prop-types";
 const AnchorHeader = ({ level, anchor, children }, { getId }) => {
   let id = getId(anchor, children);
 
-  return React.createElement(`h${level}`, { className: "header" }, [
-    <a className="header-anchor" href={`#${id}`} id={id} key="anchor" />,
-    <span className="text" key="anchor-text">
+  return React.createElement(`h${level}`, { className: "heading", id }, [
+    <a className="heading--anchor" href={`#${id}`} key="anchor-select">
       {children}
-    </span>,
-    <a className="header-anchor-select" href={`#${id}`} key="anchor-select">
-      #
     </a>,
   ]);
 };
