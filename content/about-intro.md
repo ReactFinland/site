7fe7124af9104c9290bddd45907008047589640b
Juho Vepsalainen <bebraw@gmail.com>
2018-01-22 16:50:14 +0100

    chore(about): Match mockup content better

--- content/about-intro.md
+++ content/about-intro.md
@@ -1,7 +1,3 @@
-The event is run by the non-profit *React Finland ry* organization – by developers, for developers.
-
-We have seen our share of better and worse events and will put the experience gained towards building the best conference we can organize.
-
 Our goal is to connect our valued speakers with our local community in the most fruitful way we can and provide Finnish hospitality to our conference visitors.
 
 We hope to create something unique by combining our northern traditions with speakers and visitors from all around the world.
--- layouts/AboutIndex.js
+++ layouts/AboutIndex.js
@@ -7,8 +7,7 @@ import description from "./description";
 const AboutIndex = ({ section }) => (
   <div className="flex">
     <div className="post-block-full">
-      <AnchorHeader level={2}>About the Organization</AnchorHeader>
-      <Markdown source={require("../content/about-organization.md")} />
+      <Markdown source={require("../content/about-intro.md")} />
 
       <AnchorHeader level={2}>The Venue</AnchorHeader>
       <Markdown source={require("../content/venue.md")} />
@@ -19,7 +18,21 @@ const AboutIndex = ({ section }) => (
       />
 
       <AnchorHeader level={2}>The Organizers</AnchorHeader>
+      <p>
+        React Finland is organized as a community effort. All of us are
+        developers and this shows in the conference.
+      </p>
       <Contacts className="organizers" items={content.organizers} />
+      <p>
+        Our goal is to connect our valued speakers with our local community in
+        the most fruitful way we can and provide Finnish hospitality to our
+        conference visitors.
+      </p>
+
+      <p>
+        We hope to create something unique by combining our northern traditions
+        with speakers and visitors from all around the world.
+      </p>
     </div>
   </div>
 );
