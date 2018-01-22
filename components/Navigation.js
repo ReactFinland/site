1705ea86a0383fe3a895a88d259e54187d1d218e
Andrey Okonetchnikov <andrey@okonet.ru>
2018-01-22 17:23:50 +0100

    Better navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,30 +1,22 @@
 import React from "react";
+import classnames from "classnames";
 import Link from "./Link";
 
-const Navigation = ({ pages, pathname, title }) =>
-  console.log(title) || (
-    <div className="nav__wrapper">
-      <input type="checkbox" className="nav__toggle" id="nav__toggle" />
-      <label className="nav__toggle-label" htmlFor="nav__toggle" />
-      <nav className="nav">
-        {pages.map(
-          (link, i) =>
-            link.url === pathname ? (
-              <div key={`link-${i}`} className="nav__selected__link">
-                {link.title}
-              </div>
-            ) : (
-              <div key={`link-${i}`} className="nav__link">
-                {link.url ? (
-                  <Link to={link.url}>{link.title}</Link>
-                ) : (
-                  link.title
-                )}
-              </div>
-            )
-        )}
-      </nav>
-    </div>
-  );
+const Navigation = ({ pages, pathname }) => (
+  <nav className="nav">
+    <ul>
+      {pages.map((link, i) => {
+        const linkClassNames = classnames("nav--link", {
+          "nav--link_selected": link.url === pathname,
+        });
+        return (
+          <li key={`link-${i}`} className={linkClassNames}>
+            {link.url ? <Link to={link.url}>{link.title}</Link> : link.title}
+          </li>
+        );
+      })}
+    </ul>
+  </nav>
+);
 
 export default Navigation;
