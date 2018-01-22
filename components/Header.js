e030f273e21f4557c96c0279e19eca0afd1ea734
Juho Vepsalainen <bebraw@gmail.com>
2018-01-22 10:28:46 +0100

    fix: Link from site name to index

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,10 @@ const Header = ({ pathname, logo }) => (
       <div className="logo">
         <img src={logo} width={200} height={200} alt="React Finland logo" />
       </div>
-      <div className="rubric">
-        <span>React </span>
+      <a href="/" className="rubric site-name">
+        <span className="text-rf-white">React </span>
         <span className="text-rf-blue">Finland</span>
-      </div>
+      </a>
       <h1>One workshop day + two days of talks</h1>
       <h3>Your chance to learn more about React up north</h3>
       <h3>Helsinki (24-26.4.2018)</h3>
