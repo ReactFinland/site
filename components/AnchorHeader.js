8d5801b9e55469c3daa1756922ee3a6549e7af82
Juho Vepsalainen <bebraw@gmail.com>
2018-01-22 17:11:40 +0100

    fix: Fix link inside link issue for Contact

--- components/AnchorHeader.js
+++ components/AnchorHeader.js
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const AnchorHeader = ({ level, anchor, children }, { getId }) => {
+const AnchorHeader = ({ level, anchor, children, ...props }, { getId }) => {
   let id = getId(anchor, children);
 
-  return React.createElement(`h${level}`, { className: "heading", id }, [
-    <a className="heading--anchor" href={`#${id}`} key="anchor-select">
-      {children}
-    </a>,
-  ]);
+  return React.createElement(
+    `h${level}`,
+    { className: "heading", ...props, id },
+    [
+      <a className="heading--anchor" href={`#${id}`} key="anchor-select">
+        {children}
+      </a>,
+    ]
+  );
 };
 AnchorHeader.contextTypes = {
   getId: PropTypes.func,
