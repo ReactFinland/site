92f3a7effaf78b3481cb052c484f0b640acf608b
Juho Vepsalainen <bebraw@gmail.com>
2018-01-23 11:49:53 +0100

    chore: Add missing alt attributes to images

--- components/VenueGallery.js
+++ components/VenueGallery.js
@@ -7,26 +7,31 @@ const PHOTOS = [
     src: require("assets/img/venue/confetti.jpg"),
     width: 2,
     height: 1,
+    alt: "Confetti",
   },
   {
     src: require("assets/img/venue/atmosphere.jpg"),
     width: 1,
     height: 1,
+    alt: "Atmosphere",
   },
   {
     src: require("assets/img/venue/bar.jpg"),
     width: 1,
     height: 1,
+    alt: "Bar",
   },
   {
     src: require("assets/img/venue/dj.jpg"),
     width: 1.5,
     height: 1,
+    alt: "DJ",
   },
   {
     src: require("assets/img/venue/gig.jpg"),
     width: 1.5,
     height: 1,
+    alt: "Gig",
   },
 ];
 
