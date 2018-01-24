a837148017498375a0fbdefd17dfd858efde48f1
Andrey Okonetchnikov <andrey@okonet.ru>
2018-01-24 11:26:20 +0100

    Fine tune tito form

--- layouts/SiteIndex.js
+++ layouts/SiteIndex.js
@@ -12,7 +12,7 @@ const SiteIndex = ({ section }) => (
       <Contacts items={content.speakers} render={ContactMini} />
     </div>
     <AnchorHeader level={2}>Tickets</AnchorHeader>
-    <div className="grid--full">
+    <div className="grid--5col">
       <tito-widget event="react-finland/2018" />
     </div>
   </Fragment>
