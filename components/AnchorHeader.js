e60ba6c2cae1897c8df8cc682404fac8c0cdabbf
Juho Vepsalainen <bebraw@gmail.com>
2018-02-12 14:28:44 +0100

    fix(anchor-header): Fix anchor in anchor warning

--- components/AnchorHeader.js
+++ components/AnchorHeader.js
@@ -1,18 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const AnchorHeader = ({ level, anchor, children, ...props }, { getId }) => {
+const AnchorHeader = ({ level, anchor, children }, { getId }) => {
   let id = getId(anchor, children);
 
-  return React.createElement(
-    `h${level}`,
-    { className: "heading", ...props, id },
-    [
-      <a className="heading--anchor" href={`#${id}`} key="anchor-select">
-        {children}
-      </a>,
-    ]
-  );
+  return React.createElement(`h${level}`, { className: "heading" }, [
+    <a className="heading--anchor" href={`#${id}`} id={id} key="anchor" />,
+    <span className="text" key="anchor-text">
+      {children}
+    </span>,
+    <a className="heading--anchor-select" href={`#${id}`} key="anchor-select">
+      #
+    </a>,
+  ]);
 };
 AnchorHeader.contextTypes = {
   getId: PropTypes.func,
