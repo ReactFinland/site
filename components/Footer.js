c184fd52fcfbec4c39cb6efa7ae70d3817a7bf0c
Manuel Penaloza <manpenaloza@AGITM014.local>
2018-02-19 20:18:42 +0100

    fix footer styles

--- components/Footer.js
+++ components/Footer.js
@@ -8,18 +8,12 @@ const Footer = ({ pathname }) => {
     <footer className="footer">
       <div className="footer--wrapper">
         <Navigation pathname={pathname} pages={navigationPages} />
-        <p className="footer--toolbox">
+        <section className="footer--bottomLinks">
           <a href="https://toolbox.finland.fi/">Finland Toolbox</a>
-        </p>
-        <p className="footer--imprint">
           <a href="/imprint/">Imprint</a>
-        </p>
-        <p className="footer--privacy-policy">
           <a href="/privacy-policy/">Privacy Policy</a>
-        </p>
-        <p className="footer--coc">
           <a href="http://berlincodeofconduct.org/">Code of Conduct</a>
-        </p>
+        </section>
         <p className="footer--copyright">
           © React Finland, {new Date().getFullYear()}
         </p>
