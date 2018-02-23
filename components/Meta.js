2be7bfaca92fc3643846f31282f017cbbd5cd518
Juho Vepsalainen <bebraw@gmail.com>
2018-02-23 17:02:57 +0100

    chore: Update dependencies

--- components/Meta.js
+++ components/Meta.js
@@ -11,7 +11,7 @@ const Meta = ({ siteName, title, description, keywords = [] }) => (
       {siteName} - {title}
     </title>
     <meta property="og:site_name" content={siteName} />
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui"
