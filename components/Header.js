ee38118a0a0060c5290bdbfb2855bd79e6a5ec47
Juho Vepsalainen <bebraw@gmail.com>
2018-05-29 19:07:15 +0200

    chore: Comment out unused code

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import Link from "./Link";
 import Navigation from "./Navigation";
 import logo from "assets/img/logo.svg";
 import navigationPages from "./navigation-pages";
-import Countdown from "./Countdown";
-import Interactive from "antwar-interactive";
+/*import Countdown from "./Countdown";
+import Interactive from "antwar-interactive";*/
 
 const Header = ({ pathname, title }) => {
   const isHomePage = pathname === "/";
