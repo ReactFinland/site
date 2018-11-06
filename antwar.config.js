2f919419e2248d433f28563ac81d538b48b161fd
Juho Vepsalainen <bebraw@gmail.com>
2018-11-06 20:53:46 +0200

    fix: Define meta at config
    This whole config should likely go away if a server controls the site
    and we render from that.

--- antwar.config.js
+++ antwar.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const {
-  content: { pages },
-} = require("@react-finland/content-2018");
+const EVENT_NAME = "React Finland";
 
 module.exports = {
   apiUrl: "http://localhost:4000/graphql", // "https://api.react-finland.fi/graphql",
@@ -20,29 +18,55 @@ module.exports = {
       description: "Page was not found",
       title: "Page not found",
     }),
-    "/": page("index"),
+    "/": page("index", {
+      title: EVENT_NAME,
+      description: `${EVENT_NAME} (24-26.04.2019, Helsinki) is a conference that comes with a workshop day and two days of talks.`,
+    }),
     "2018": page("2018", {
       description: "React Finland 2018",
       title: "React Finland 2018",
     }),
-    "for-attendees": page("for-attendees"),
-    "for-sponsors": page("for-sponsors"),
-    about: page("about"),
-    imprint: page("imprint"),
-    "privacy-policy": page("privacy-policy"),
-    "2018/schedule": page("schedule"),
-    "2018/speakers": page("speakers"),
-    "2018/workshops": page("workshops"),
+    "for-attendees": page("for-attendees", {
+      title: "For Attendees",
+      description: `What should I know as a ${EVENT_NAME} attendee`,
+    }),
+    "for-sponsors": page("for-sponsors", {
+      title: "For Sponsors",
+      description: `What should I know as a ${EVENT_NAME} sponsor?`,
+    }),
+    about: page("about", {
+      title: "Organizers",
+      description: `Who is organizing ${EVENT_NAME}?`,
+    }),
+    imprint: page("imprint", {
+      title: "Imprint",
+      description: `How to reach ${EVENT_NAME} organizers?`,
+    }),
+    "privacy-policy": page("privacy-policy", {
+      title: "Privacy Policy",
+      description: `What is the privacy policy of ${EVENT_NAME}?`,
+    }),
+    "2018/schedule": page("schedule", {
+      title: "Schedule",
+      description: `What is the schedule of ${EVENT_NAME}?`,
+    }),
+    "2018/speakers": page("speakers", {
+      title: "Speakers",
+      speakers: `Who is going to speak at ${EVENT_NAME}?`,
+    }),
+    "2018/workshops": page("workshops", {
+      title: "Workshops",
+      description: `Which workshops will be held at ${EVENT_NAME}?`,
+    }),
   },
 };
 
-function page(name, meta) {
+function page(name, meta = {}) {
   const ret = () => {
     const pageComponent = require(`./pages/${name}`).default;
-    const pageData = pages.find(({ id }) => id === name) || {};
 
-    pageComponent.description = pageData.description || meta.description;
-    pageComponent.title = pageData.title || meta.title;
+    pageComponent.description = meta.description;
+    pageComponent.title = meta.title;
 
     return pageComponent;
   };
