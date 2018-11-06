b32e9351a2afd2790787dff7c1f5b324d68f630c
Juho Vepsalainen <bebraw@gmail.com>
2018-11-06 20:15:53 +0200

    fix: Use the new API for privacy policy

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -1,22 +1,46 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Markdown, connect } from "components";
+import { Markdown } from "components";
 
-const PrivacyPolicy = ({ page: { main } }) => (
+const main = `
+GraphQL Finland is committed to protecting your privacy. This privacy policy represents our privacy practices. Please take time to review our practices. If there are any practice matters that do not suit your consent, we recommend not using this site.
+
+## Collecting information
+
+We collect information from you when you register on our site or fill out a form. When filling out our contact form you may be asked to enter your name, e-mail, and phone number. Please do not submit your personal information if you would not like us to collect it.
+
+## Using your information
+
+We will use your personal information for the following purposes:
+
+* Personalizing our website. We would like to create the best experience for you. By using your personal information, we are able to cater our site towards your needs.
+* Improving our website. Through the information and feedback given, we are able to improve our site thanks to your insights.
+* Improving customer service. Your information will allow us to create a more effective customer service experience by utilizing the data provided.
+
+## Outside parties
+
+We do not disclose, trade, sell, or transfer any of your personal information to any outside parties.
+
+## Protecting your information
+
+We use a number of security measures to ensure the safety and privacy of your personal information.
+
+## Cookies
+
+We also take advantage of the use of cookies. Cookies are small pieces of information that are placed by your computer through the websites you visit. This creates a unique footprint for you without giving away your identity.
+
+The use of cookies allows us to do our job more effectively and make sure that we provide you with all your relevant searches.
+
+We use Google Analytics cookies for our site, in order to collect statistics on visitors of the site. This helps us to track visitor’s habits and have a better understanding on how people use our website.
+
+Most web browsers accept cookies automatically, however, you may alter your browser’s setting to prevent this from happening.
+`;
+
+const PrivacyPolicy = () => (
   <section className="intro intro_privacy">
     <div className="intro--main">
       <Markdown source={main} />
     </div>
   </section>
 );
-PrivacyPolicy.propTypes = {
-  page: PropTypes.object,
-};
 
-export default connect(`
-{
-  page(id: "privacy-policy") {
-    main
-  }
-}
-`)(PrivacyPolicy);
+export default PrivacyPolicy;
