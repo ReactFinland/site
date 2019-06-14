f19b9d01328f8de63332184e7af17ffe2ce4dfed
Juho Vepsalainen <bebraw@gmail.com>
2019-06-14 11:30:03 +0200

    chore: Improve for sponsors page

--- pages/for-sponsors.js
+++ pages/for-sponsors.js
@@ -16,7 +16,7 @@ We expect the audience to contain plenty of international flavor. Our target is 
 
 ## National and International Visibility
 
-The conference will have visibility both in national and international level and will reach the ecosystem in React and React Native widely. It has been positioned as a non-profit, developers to developers kind of conference but we are willing to provide visibility to limited amount of companies in order to produce the highest quality conference we can.
+The conference will have visibility both in national and international level and will reach the ecosystem in React and React Native widely. It has been positioned as a developers to developers conference but we are willing to provide visibility to limited amount of companies in order to produce the highest quality conference we can. We see sponsorships as value add and believe good collaborations can improve the experience for everyone involved.
 
 ## Your Spot to Shine
 
@@ -26,7 +26,7 @@ We are offering the perfect spot for companies to promote their knowledge and pr
 
 | | BRONZE | SILVER | GOLD
 | - | - | - |
-| Cost | 2.000 € | 5.000 € | 10.000 €
+| Cost (VAT 0) | 2.000 € | 5.000 € | 10.000 €
 | Available | 8 | 4 | 3
 | Conference tickets | 2 x &#9786; | 4 x &#9786; | 6 x &#9786;
 | Visibility on social media and website | &#9786; | &#9786; &#9786; | &#9786; &#9786; &#9786;
@@ -36,10 +36,10 @@ We are offering the perfect spot for companies to promote their knowledge and pr
 | Speaker area access | &#10003; | &#10003; | &#10003;
 | Logo on slides between the talks | &#10003; | &#10003; | &#10003;
 | Job announcements | | &#10003; | &#10003;
-| Area in sponsor lounge for booths / promotion | | | &#10003;
+| Area in sponsor lounge for booths / promotion | | &#10003; | &#10003;
 | Sticker in goodie bag | &#10003; | &#10003; | &#10003;
 | Item in the goodie bag | | | &#10003;
-| Attendance for the speaker events (e.g. the speakers dinner) | | | &#9786;
+| Attendance for the speaker events (e.g. the speakers' dinner) | | | &#9786;
 | Special requests | | | &#10003;
 
 ## How to Sponsor?
