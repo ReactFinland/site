3d7c833cd222292b392ee4e3955516fd327aba0a
Juho Vepsalainen <bebraw@gmail.com>
2019-11-19 16:06:30 +0100

    chore: Fix links

--- pages/2018.js
+++ pages/2018.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Markdown } from "components";
 
 const main = `
-React Finland was a great success! Read about [what lessons we learned](https://medium.com/react-finland/react-finland-2018-lessons-learned-754a5f29067c) and [how people experienced the conference](https://medium.com/react-finland/react-finland-2018-blog-coverage-e65bcece8946).
+React Finland was a great success! Read about [what lessons we learned](/blog/react-finland-2018---lessons-learned-754a5f29067c/) and [how people experienced the conference](/blog/react-finland-2018---blog-coverage-e65bcece8946/).
 
 See also [2018 speakers](/2018/speakers/), [schedule](/2018/schedule/), and [workshops](/2018/workshops/).`;
 
