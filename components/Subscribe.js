b1a9d1f01bf04d25505615db3e11d1f77346b43c
Juho Vepsäläinen <bebraw@gmail.com>
2020-04-23 16:34:05 +0300

    chore: Add Mailjet subscription form

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -5,51 +5,42 @@ const Subscribe = ({
   children = "Subscribe to get React Finland related news to your mail.",
 }) => {
   return (
-    <section className="subscribe">
-      <div className="subscribe--wrapper">
-        <h2 className="subscribe--header">{children}</h2>
-
-        <form
-          action="//react-finland.us16.list-manage.com/subscribe/post?u=a940d62db3f360204bf40b1c4&amp;amp;id=8c82fd10b8"
-          method="post"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
-          className="subscribe--form"
-          target="_blank"
-          noValidate
+    <section
+      className="subscribe"
+      style={{ padding: "2em", textAlign: "center" }}
+    >
+      <data
+        id="mj-w-res-data"
+        data-token="fd098aa40e8dccbaa2dd415c3003999f"
+        className="mj-w-data"
+        data-apikey="5h9R"
+        data-w-id="qG4"
+        data-lang="en_US"
+        data-base="https://app.mailjet.com"
+        data-width="640"
+        data-height="383"
+        data-statics="statics"
+      ></data>
+      <div
+        className="mj-w-button mj-w-btn"
+        style={{
+          fontFamily: "Finlandica, Helvetica",
+          color: "#1863bd",
+          padding: "0px 1em",
+          backgroundColor: "rgb(100%, 100%, 100%, 0.7)",
+          display: "inline-block",
+          borderRadius: "25px",
+        }}
+      >
+        <div
+          className="mj-w-button-content"
+          style={{
+            fontFamily: "Finlandica, Helvetica",
+            display: "inline-block",
+          }}
         >
-          <input
-            type="email"
-            placeholder="john@domain.com"
-            name="EMAIL"
-            className="subscribe--input"
-            id="mce-EMAIL"
-            required
-            aria-label="email"
-          />
-          <div
-            style={{
-              display: "none",
-              position: "absolute",
-              left: "-5000px",
-            }}
-          >
-            <input
-              type="text"
-              name="b_ed40c0084a0c5ba31b3365d65_b853b8e786"
-              tabIndex="-1"
-              value=""
-            />
-          </div>
-          <button
-            type="submit"
-            className="subscribe--button"
-            name="subscribe"
-            id="mc-embedded-subscribe"
-          >
-            Subscribe
-          </button>
-        </form>
+          {children}
+        </div>
       </div>
     </section>
   );
