9dfae3dc5a70d23ee504246ece9c8353c1a69b04
Juho Vepsäläinen <bebraw@gmail.com>
2022-03-15 13:45:28 +0200

    fix: Fix wording

--- layouts/SiteBody.js
+++ layouts/SiteBody.js
@@ -92,7 +92,7 @@ const SiteBody = (
                     className="sponsors--heading visually-hidden"
                     level={2}
                   >
-                    Sponsors of 2019
+                    Sponsors
                   </AnchorHeader>
                 </div>
                 <div className="sponsors sponsors_gold">
