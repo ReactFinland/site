bc21c9bf3580b29d8e7c329a4443bbb7a520ffad
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-08 19:31:32 +0300

    chore: Re-enable bold

--- components/navigation-pages.js
+++ components/navigation-pages.js
@@ -39,7 +39,7 @@ export default [
         url: "/workshops/",
       },
       {
-        title: "Register", // Work around Fienta bug <b>Register</b>,
+        title: <b>Register</b>,
         url: "https://fienta.com/react-finland-2022",
       },
     ],
