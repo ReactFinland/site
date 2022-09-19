11d92fa86ac71afc181c175ce872f5dcf13b76f5
Juho Vepsäläinen <bebraw@gmail.com>
2022-09-19 16:03:02 +0300

    fix: Fix typo

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import {
   Sponsors,
 } from "components";
 
-const intro = `React Finland 2022 occured between 12th and 16th of September as a live conference at Paasitorni, Helsinki. The workshops take place between 12th and 14th and the conference itself is held on 15th and 16th.
+const intro = `React Finland 2022 occurred between 12th and 16th of September as a live conference at Paasitorni, Helsinki. The workshops take place between 12th and 14th and the conference itself is held on 15th and 16th.
 
 [To learn about the event, read our launch post.](/blog/rf-2022/)
 
